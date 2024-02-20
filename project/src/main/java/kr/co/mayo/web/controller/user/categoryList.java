d2b820b64eb1c4f5a06046ffc584bb3987457960
Ryun Ji <ryunji1990@gmail.com>
2024-02-20 09:22:30 +0900

    프로젝트 기본 소스 커밋

---
+++ project/src/main/java/kr/co/mayo/web/controller/user/categoryList.java
@@ -0,0 +1,5 @@
+package kr.co.mayo.web.controller.user;
+
+public class categoryList {
+
+}
---
+++ project/src/main/java/kr/co/mayo/web/entity/Menu.java
@@ -0,0 +1,22 @@
+package kr.co.mayo.web.entity;
+
+//
+public class Menu {
+
+	private int id;
+	private String korName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getKorName() {
+		return korName;
+	}
+	public void setKorName(String korName) {
+		this.korName = korName;
+	}
+	
+	
+}
